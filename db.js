a62209e24ab32294ea3990a59f41c4c102fa4945
antonwy <anton.wyrowski@gmail.com>
2019-02-08 19:24:29 +0100

    added mongodb

--- db.js
+++ db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://antonwy:bo5WD4banmwiyPxk@chatdb-noqrj.mongodb.net/test?retryWrites=true')
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on('error', (err) => {
+  console.log(err)
+});
 db.once('open', function() {
   console.log("Connected to mongodb")
 });
